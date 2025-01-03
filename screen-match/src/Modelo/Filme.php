b01effadca47355293d08b85cfc59236dc4f3326
GabrielNP96 <gabrielnp1996@gmail.com>
2025-01-02 23:56:23 -0300

    Controlando o acesso

--- screen-match/src/Modelo/Filme.php
+++ screen-match/src/Modelo/Filme.php
@@ -1,8 +1,20 @@
 <?php
 
 class Filme {
-    public string $nome;
-    public int $anoDelancamento;
-    public string $genero;
-    public float $nota;
+    private string $nome;
+    private int $anoDelancamento;
+    private string $genero;
+    private float $nota;
+    private array $notas =  [];
+
+    function avalia(float $nota): void {
+        $this->notas[] = $nota;
+    }
+
+    function media(): float {
+        $somaNotas = array_sum($this->notas);
+        $quantidadeDeNotas = count($this->notas);
+
+        return $somaNotas / $quantidadeDeNotas;
+    }
 }
